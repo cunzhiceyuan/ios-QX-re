8c5733f06596a14b8113bdd24139ab321dc15ac2
寸指测渊 <48240407+cunzhiceyuan@users.noreply.github.com>
2024-07-28 07:05:08 +0800

    更新 vless.js

---
+++ vless.js
@@ -0,0 +1,7 @@
+vless=www.visa.com.sg:8880, method=none, password=015f11ee-271a-4a74-bbb5-2275383c1189, obfs=ws, obfs-host=worker-sweet-violet-95bf.xdzq8qzvbw.workers.dev, obfs-uri=/?ed=2560, fast-open=false, udp-relay=false, tag=worker-sweet-violet-95bf.xdzq8qzvbw.workers.dev
+
+
+vless=	104.19.242.236:80, method=none, password=ce4a253c-dd4f-40a4-8971-1b507a130fca, obfs=ws, obfs-host=dongjingip.alt-cu-9ops48nl.workers.dev, obfs-uri=/?ed=2048, fast-open=false, udp-relay=false, tag=dongjingip.alt-cu-9ops48nl.workers.dev
+vless=104.16.88.102:8880, method=none, password=9979b74a-99e2-4561-8e4f-8f3f81d256a5, obfs=ws, obfs-host=yg.j1249845202.workers.dev, obfs-uri=/?ed=2048, fast-open=false, udp-relay=false, tag=yg.j1249845202.workers.dev
+#根据上面的vless更改，反代ip，ip来源：https://stock.hostmonit.com/CloudFlareYes
+vless=104.19.242.236:8880, method=none, password=9979b74a-99e2-4561-8e4f-8f3f81d256a5, obfs=ws, obfs-host=yg.j1249845202.workers.dev, obfs-uri=/?ed=2048, fast-open=false, udp-relay=false, tag=1.j1249845202.workers.dev
