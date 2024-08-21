6e0d19130c03094ae20b77d90915a0f863ae75fa
寸指测渊 <48240407+cunzhiceyuan@users.noreply.github.com>
2024-08-21 11:08:27 +0800

    Update AppRaven.js

--- AppRaven.js
+++ AppRaven.js
@@ -6,7 +6,7 @@ AppRaven
 [rewrite_local]
 
 
-https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/cunzhiceyuan/ios-QX-re/main/AppRaven.js
+https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/AppRaven.js
 [mitm] 
 
 hostname = appraven.net
