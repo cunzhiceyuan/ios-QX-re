b2784ce011c3e62567054c31261d03c8f45f7ba2
寸指测渊 <48240407+cunzhiceyuan@users.noreply.github.com>
2024-08-31 11:22:26 +0800

    Update chavyleung-scripts-master-10000-10000.cookie.js

--- chavyleung-scripts-master-10000-10000.cookie.js
+++ chavyleung-scripts-master-10000-10000.cookie.js
@@ -1,1 +1,2 @@
 ^https:\/\/wapside.189.cn:9001\/api\/home\/homeInfo url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/10000/10000.cookie.js
+hostname =wapside.189.cn
