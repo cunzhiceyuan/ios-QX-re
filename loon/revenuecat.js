4b3a22cad28900b638dddd98962f8dd83eea0590
寸指测渊 <48240407+cunzhiceyuan@users.noreply.github.com>
2024-09-27 07:27:02 +0800

    Update revenuecat.js

---
+++ loon/revenuecat.js
@@ -0,0 +1,16 @@
+#!name=Revenuecat
+#!desc=Revenuecat
+#!icon=https://github.com/Toperlock/Quantumult/raw/main/icon/Doraemon/Doraemon-1063.png
+
+[Rewrite]
+https://api.lianjiu.fun/app/api/v1/profile - reject
+
+[Script]
+#修改
+http-response ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) script-path=https://raw.githubusercontent.com/Yu9191/Rewrite/main/Revenuecat.js, requires-body=true, timeout=60, tag=Revenuecat
+
+#清理
+http-request ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) script-path=https://raw.githubusercontent.com/Yu9191/Rewrite/main/Revenuecat.js, timeout=60, tag=Revenuecat
+
+[MITM]
+hostname = api.revenuecat.com, api.lianjiu.fun
