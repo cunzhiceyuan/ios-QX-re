f18235110d3f58cc8185e7c275bdb4ec89ab14c6
寸指测渊 <48240407+cunzhiceyuan@users.noreply.github.com>
2024-09-27 16:46:16 +0800

    Update yjt.js

--- loon/js/yjt.js
+++ loon/js/yjt.js
@@ -3,7 +3,7 @@
  * 解锁会员
  
 [rewrite_local]
-^https?:\/\/1jietu\.com\/apiv2\/(user|ad) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/yjt.js
+^https?:\/\/1jietu\.com\/apiv2\/(user|ad) url script-response-body https://raw.githubusercontent.com/cunzhiceyuan/ios-QX-re/refs/heads/main/loon/js/yjt.js
 
 [mitm]
 hostname = 1jietu.com
