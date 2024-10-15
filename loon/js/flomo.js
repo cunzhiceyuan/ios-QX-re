24a39a3bad874d2481f547183970d684e8abc9db
cunzhiceyuan <48240407+cunzhiceyuan@users.noreply.github.com>
2024-10-15 22:09:43 +0800

    Update flomo.js

--- loon/js/flomo.js
+++ loon/js/flomo.js
@@ -1,7 +1,8 @@
-* flomo
+/*
+ * flomo
 
 [rewrite_local]
-^https:\/\/flomoapp\.com\/api\/v1\/user\/me url script-analyze-echo-response https://raw.githubusercontent.com/cunzhiceyuan/ios-QX-re/refs/heads/main/loon/js/flomo.js
+^https:\/\/flomoapp\.com\/api\/v1\/user\/me url script-analyze-echo-response flomo.js
 
 [mitm]
 hostname = flomoapp.com
@@ -16,4 +17,4 @@ const response = {
     },
     body: '{"message":"","data":{"id":1,"apple_mac_subscription":null,"referee_pro_days":9999,"register_via":"ios","apple_subscription":null,"created_at":"2010-07-22 15:20:50","wechat_nickname":null,"email_verified_at":"2023-07-22 15:20:50","pro_expired_at":"2099-09-09 23:59:59","api_token":"","language":"zh","slug":"baby","google_play_subscription":null,"referer_id":null},"code":0}'
 }
-$done(isQuanX ? response : { response })l
+$done(isQuanX ? response : { response })
